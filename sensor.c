fd30ead40a9666b4a9c6939575bb59f8e19003ab
Willy19921992 <guuillekarateka@gmail.com>
2024-09-23 22:46:44 +0200

    Asegurar division de double y usar constantes predefinidas

--- sensor.c
+++ sensor.c
@@ -3,7 +3,6 @@
 
 #define MIN_VALUE 5.0
 #define MAX_VALUE 40.0
-#define PI 3.141592653589793
 
 int main() {
     // Parameters
@@ -33,7 +32,7 @@ int main() {
     // Generate sinusoidal values and write to the CSV file
     for (int i = 0; i < num_samples; i++) {
         // Generate a sinusoidal value in the range [0, 2*PI]
-        double sine_value = sin(2 * PI * i / num_samples);
+        double sine_value = sin(2 * M_PI * (double)i / num_samples);
 
         // Scale the value to fit between 5 and 40
         double scaled_value = MIN_VALUE + (sine_value + 1.0) * (MAX_VALUE - MIN_VALUE) / 2.0;
@@ -46,6 +45,6 @@ int main() {
     fclose(file);
 
     printf("Sinusoidal data written to 'sinusoidal_data.csv'.\n");
-    
+
     return 0;
 }
