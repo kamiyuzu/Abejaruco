c219b17b05154aea69707b802b8a6931bee5b689
kamiyuzu <alberto.rev.a@gmail.com>
2024-09-25 00:04:51 +0200

    Fix noise reduction decons

--- src/NoiseReduction.cpp
+++ src/NoiseReduction.cpp
@@ -8,6 +8,8 @@ NoiseReduction::NoiseReduction(unsigned int window_size, const std::vector<doubl
     output(input.size(), 0)
 {}
 
+NoiseReduction::~NoiseReduction(){}
+
 // TODO: Implement noise reduction algorithm
 const std::vector<double>& NoiseReduction::denoise() {
     return this->output;
