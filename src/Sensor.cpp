e7169f449fef17e4037f28566fab4e37ccb53078
Willy19921992 <guuillekarateka@gmail.com>
2024-10-09 11:05:16 +0200

    Sensor: Primera implementacion basica

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -1,41 +1,25 @@
 #include "Sensor.hpp"
-#include <algorithm>
+#include <string>
+#include <variant>
 #include <vector>
-#include <random>
+#include <cmath>
 
 // Constructor with optional parameters, as they have default values
-Sensor::Sensor(double MIN_VALUE, double MAX_VALUE, unsigned int num_samples, unsigned int period,
-    unsigned int adc_resolution, unsigned int adc_noise_lsb, double sensor_noise_c) :
-        min_value(MIN_VALUE),
-        max_value(MAX_VALUE),
-        num_samples(num_samples),
-        period(period),
-        adc_noise_lsb(adc_noise_lsb),
-        sensor_noise_c(sensor_noise_c),
-        adc_step_size((MAX_VALUE - MIN_VALUE) / (1 << adc_resolution))
+Sensor::Sensor() :
+        min_value(0.0),
+        max_value(50.0),
+        num_samples(10000),
+        period(10000),
+        adc_noise_lsb(2),
+        adc_resolution(10),
+        sensor_noise_c(2.0),
+        adc_step_size(50.0 / (1 << 10))
 {}
 
 Sensor::~Sensor(){}
 
-/*  El ruido en las mediciones proviene de dos fuentes:
-        1. Ruido y resolución del ADC: Podemos tomar como referencia el ADC del Arduino UNO
-           Tiene una resolución de 10-bits con una precisión absoluta de +-2 LSB.
-        2. Ruido típico del sensor de temperatura: Como referencia se puede usar el DHT11 que
-           viene incluido con el Arduino UNO, tiene un rango de medición de 0ºC a 50ºC con el
-           mismo voltaje que el ADC, con una precisión de +-2ºC.
-    Esto significa que el ADC tiene una resolución de 50 / 2¹⁰ = 0,048828125ºC
-    y un error de +-0,09765625ºC.
-
-    Entonces el ruido se puede calcular como:
-        Temp + (+-2 * 0,048...) + (floor(+-2 / 0,048...) * 0,048...)
-    Que significa que la temperatura medida es la real + la precisión del ADC +
-    la precisión del sensor (con un tamaño de paso igual a la resolución del sensor)
-*/
-std::vector<double> Sensor::genData() {
+std::vector<double> Sensor::execute() {
     std::vector<double> data(this->num_samples, 0.0);
-    // Initialize random seed for noise generation using random_device and mt19937
-    std::random_device rd;
-    std::mt19937 gen(rd());
 
     // Generate sinusoidal values
     for (unsigned int i = 0; i < this->num_samples; i++) {
@@ -43,20 +27,67 @@ std::vector<double> Sensor::genData() {
         double sine_value = sin(2 * M_PI * static_cast<double>(i) / this->period);
 
         // Scale the value to fit between MIN and MAX
-        double scaled_value = min_value + (sine_value + 1.0) * (max_value - min_value) / 2.0;
-
-        // Noise from ADC
-        std::uniform_int_distribution<> adc_dis(-this->adc_noise_lsb, this->adc_noise_lsb);
-        double adc_noise = static_cast<double>(adc_dis(gen)) * this->adc_step_size;
-
-        // Noise from sensor
-        std::uniform_real_distribution<> sensor_dis(-this->sensor_noise_c, this->sensor_noise_c);
-        double sensor_noise = std::floor(sensor_dis(gen) / this->adc_step_size) * this->adc_step_size;
-
-        // Clamp and assign the value
-        scaled_value += adc_noise + sensor_noise;
-        data[i] = std::clamp(scaled_value, this->min_value, this->max_value);
+        data[i] = min_value + (sine_value + 1.0) * (max_value - min_value) / 2.0;
     }
 
     return data;
+}
+
+const std::vector<std::string> Sensor::getParameters() {
+    return std::vector<std::string>{ "min_value", "max_value", "num_samples", "period", "adc_noise_lsb", "sensor_noise_c", "adc_resolution" };
+}
+
+int Sensor::setParameter(const std::string& key, const ParamType& value) {
+    if (!key.compare("min_value")) {
+        if (std::holds_alternative<double>(value)) {
+            this->min_value = std::get<double>(value);
+            this->adc_step_size = (this->max_value - this->min_value) / (1 << this->adc_resolution);
+        }
+        else
+            return 1;
+    }
+    else if (!key.compare("max_value")) {
+        if (std::holds_alternative<double>(value)) {
+            this->max_value = std::get<double>(value);
+            this->adc_step_size = (this->max_value - this->min_value) / (1 << this->adc_resolution);
+        }
+        else
+            return 1;
+    }
+    else if (!key.compare("num_samples")) {
+        if (std::holds_alternative<unsigned int>(value))
+            this->num_samples = std::get<unsigned int>(value);
+        else
+            return 1;
+    }
+    else if (!key.compare("period")) {
+        if (std::holds_alternative<unsigned int>(value))
+            this->period = std::get<unsigned int>(value);
+        else
+            return 1;
+    }
+    else if (!key.compare("adc_noise_lsb")) {
+        if (std::holds_alternative<unsigned int>(value))
+            this->adc_noise_lsb = std::get<unsigned int>(value);
+        else
+            return 1;
+    }
+    else if (!key.compare("sensor_noise_c")) {
+        if (std::holds_alternative<double>(value))
+            this->sensor_noise_c = std::get<double>(value);
+        else
+            return 1;
+    }
+    else if (!key.compare("adc_resolution")) {
+        if (std::holds_alternative<unsigned int>(value)) {
+            this->adc_resolution = std::get<unsigned int>(value);
+            this->adc_step_size = (this->max_value - this->min_value) / (1 << this->adc_resolution);
+        }
+        else
+            return 1;
+    }
+    else
+        return 2;
+
+    return 0;
 }
--- src/Sensor.hpp
+++ src/Sensor.hpp
@@ -1,22 +1,21 @@
+#include "PluginInterface.hpp"
 #include <vector>
-class Sensor {
+
+class Sensor : public PluginInterface {
     public:
         // Constructor with default parameters
-        Sensor(double MIN_VALUE = 0.0,
-                double MAX_VALUE = 50.0,
-                unsigned int num_samples = 10000,
-                unsigned int period = 10000,
-                unsigned int adc_resolution = 10,
-                unsigned int adc_noise_lsb = 2,
-                double sensor_noise_c = 2.0);
+        Sensor();
         ~Sensor();
-        std::vector<double> genData();
+        std::vector<double> execute();
+        const std::vector<std::string> getParameters();
+        int setParameter(const std::string& key, const ParamType& value);
     private:
         double min_value;
         double max_value;
         unsigned int num_samples;
         unsigned int period;
         unsigned int adc_noise_lsb;
+        unsigned int adc_resolution;
         double sensor_noise_c;
         double adc_step_size;
 };
